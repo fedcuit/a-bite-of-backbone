2241f87dddd431b0b68f3af8192c8bd79dfcd44a
Erdong Feng <edfeng@thoughtworks.com>
2015-06-05 09:17:24 +0800

    enable toggle all todo items.(collection.each to change state of all models and view.listenTo('change') to update each view)

--- scripts/views/todoItemView.js
+++ scripts/views/todoItemView.js
@@ -7,6 +7,7 @@ myTodoApp.TodoItemView = Backbone.View.extend({
     initialize: function () {
         this.render();
         this.listenTo(this.model, "destroy", this.remove);
+        this.listenTo(this.model, "change", this.render);
     },
     render: function () {
         return this.$el.html(this.template(this.model.attributes));
--- scripts/views/todoListView.js
+++ scripts/views/todoListView.js
@@ -3,13 +3,15 @@ var myTodoApp = myTodoApp || {};
 myTodoApp.TodoListView = Backbone.View.extend({
     el: $("#mainContent"),
     initialize: function () {
+        this.$toggleAll = this.$('.toggle-all');
         this.$newItemTitle = this.$(".newItem .title");
         this.$items = this.$('.items');
         this.listenTo(todoList, "add", this.addNew);
     },
     events: {
         "click .newItem .add": "addByClick",
-        "keypress .newItem .title": "addByEnter"
+        "keypress .newItem .title": "addByEnter",
+        "change .toggle-all": "toggleAll"
     },
     addByClick: function () {
         var todoItem = new myTodoApp.TodoItem({title: this.$newItemTitle.val()});
@@ -28,5 +30,11 @@ myTodoApp.TodoListView = Backbone.View.extend({
         } else {
             alert(newItem.validationError);
         }
+    },
+    toggleAll: function () {
+        var self = this;
+        todoList.each(function (todo) {
+            todo.set('completed', self.$toggleAll.prop("checked"));
+        });
     }
 });
