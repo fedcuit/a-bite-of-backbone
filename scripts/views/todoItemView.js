28f1c5f93fb20eedf98526128f7213337683db2a
Erdong Feng <edfeng@thoughtworks.com>
2015-06-05 08:44:31 +0800

    enable tick and remove a todo item(call view.remove to remove it from dom and unbind all listeners).

--- scripts/views/todoItemView.js
+++ scripts/views/todoItemView.js
@@ -6,19 +6,20 @@ myTodoApp.TodoItemView = Backbone.View.extend({
     template: _.template($("#todoItemTemplate").html()),
     initialize: function () {
         this.render();
+        this.listenTo(this.model, "destroy", this.remove);
     },
     render: function () {
         return this.$el.html(this.template(this.model.attributes));
     },
     events: {
-        "click .toggle": "handleToggle",
+        "change .toggle": "handleToggle",
         "click .destroy": "handleDestroy"
     },
     handleToggle: function () {
-        alert("are you going to check this one?");
+        this.model.set("completed", this.$('.toggle').prop('checked'));
     },
 
     handleDestroy: function () {
-        alert("are you going to destroy this one?")
+        this.model.destroy();
     }
 });
